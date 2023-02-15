622583ca912dbfb271ef39a60cc9b33e0c916f8b
darren88881 <darren88881@outlook.com>
2023-02-15 10:25:02 +0800

    Docker-HelloWorld-修改文件目录

--- src/main/java/com/itguigu/docker/HelloApplication.java
+++ src/main/java/com/itguigu/docker/HelloApplication.java
@@ -1,4 +1,4 @@
-package com.itguigu.controller;
+package com.itguigu.docker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/itguigu/docker/controller/HelloController.java
+++ src/main/java/com/itguigu/docker/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.itguigu.controller.hello;
+package com.itguigu.docker.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
