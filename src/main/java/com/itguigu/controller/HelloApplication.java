eee212a81fe1efcfb3ce69132882badb7a83b68e
darren88881 <darren88881@outlook.com>
2023-02-15 10:20:37 +0800

    Docker-HelloWorld

---
+++ src/main/java/com/itguigu/controller/HelloApplication.java
@@ -0,0 +1,16 @@
+package com.itguigu.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @Author darren
+ * @Date 2023/2/13 21:56
+ */
+@SpringBootApplication
+public class HelloApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class, args);
+    }
+}
---
+++ src/main/java/com/itguigu/controller/hello/HelloController.java
@@ -0,0 +1,18 @@
+package com.itguigu.controller.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @Author darren
+ * @Date 2023/2/13 21:38
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
+}
